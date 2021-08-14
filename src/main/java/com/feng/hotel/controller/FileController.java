8d688102a42cbd5c333ca958c00ae923dabbdebf
风 <15890194185@163.com>
2021-08-14 16:23:12 +0800

    完善文件上传

--- src/main/java/com/feng/hotel/controller/FileController.java
+++ src/main/java/com/feng/hotel/controller/FileController.java
@@ -1,10 +1,15 @@
 package com.feng.hotel.controller;
 
+import com.feng.hotel.base.exception.BizException;
+import com.feng.hotel.base.exception.BizInfo;
+import com.feng.hotel.common.enums.HotelEnum;
 import com.feng.hotel.utils.GlobalUtils;
 import com.feng.hotel.utils.IdWorkerUtils;
+
 import java.io.File;
 import java.io.IOException;
 import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,17 +26,20 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController {
 
 
-  @PostMapping(value = "/upload")
-  public String upload(@RequestParam(value = "file") MultipartFile multipartFile) throws IOException {
-    String fileName = multipartFile.getOriginalFilename();
+    @PostMapping(value = "/upload")
+    public String upload(@RequestParam(value = "file") MultipartFile multipartFile) throws IOException {
+        String fileName = multipartFile.getOriginalFilename();
 
-    File file = new File(System.getProperty("user.dir").concat("/image/").concat(IdWorkerUtils.generateStringId())
-        .concat(GlobalUtils.getFileExt(fileName)));
-    if (!file.exists()) {
-      file.mkdirs();
+        File file = new File(System.getProperty("user.dir").concat("/image/").concat(IdWorkerUtils.generateStringId())
+            .concat(GlobalUtils.getFileExt(fileName)));
+        if (!file.exists()) {
+            if (!file.mkdirs()) {
+                throw new BizException(BizInfo.FILE_UPLOAD_ERROR);
+            }
+            ;
+        }
+
+        multipartFile.transferTo(file);
+        return file.getCanonicalPath();
     }
-
-    multipartFile.transferTo(file);
-    return file.getCanonicalPath();
-  }
 }
