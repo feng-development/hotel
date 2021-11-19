e93107375893700699f925053680e24eb0a1c874
郭亚风 <15890194185@163.com>
2021-11-19 17:25:40 +0800

    修改传参类型

--- src/main/java/com/feng/hotel/controller/RoomController.java
+++ src/main/java/com/feng/hotel/controller/RoomController.java
@@ -7,17 +7,16 @@ import com.feng.hotel.request.RoomRequest;
 import com.feng.hotel.response.RoomResponse;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
-
-import java.util.List;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * @author Administrator
@@ -36,13 +35,13 @@ public class RoomController extends BaseController {
     @PostMapping(value = "save")
     @ApiOperation(value = "添加房间")
     public Result<?> save(@RequestBody RoomRequest roomRequest) {
-        roomManager.save(roomRequest,super.getUserNo());
+        roomManager.save(roomRequest, super.getUserNo());
         return Result.success();
     }
 
-    @GetMapping(value = "list/{status}")
+    @GetMapping(value = "list")
     @ApiOperation(value = "房间列表")
-    public List<RoomResponse> list(@PathVariable(value = "status") String status) {
+    public List<RoomResponse> list(@RequestParam(value = "status", required = false) String status) {
         return roomManager.list(status);
     }
 
